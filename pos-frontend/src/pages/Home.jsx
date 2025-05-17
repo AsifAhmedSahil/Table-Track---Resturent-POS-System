6a965b7b7a5bc28bb3ef9bef69601d0189bb2873
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-05-17 11:51:21 +0600

    card component added:

--- pos-frontend/src/pages/Home.jsx
+++ pos-frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import BottomNav from "../components/shared/BottomNav";
 import Greetings from "../components/home/Greetings";
+import MiniCard from "../components/home/MiniCard";
+import { FaSackDollar } from "react-icons/fa6";
+import { GiProgression } from "react-icons/gi";
 
 const Home = () => {
   return (
@@ -8,6 +11,11 @@ const Home = () => {
       {/* left */}
       <div className="flex-[3] ">
         <Greetings />
+        <div className="flex gap-3 px-8 mt-5">
+            <MiniCard title="Total Earnings" number="512" footerNum="1.6" icon={<FaSackDollar/>}/>
+            <MiniCard title="In Progress" number="310" footerNum="2.6" icon={<GiProgression/>}/>
+            
+        </div>
       </div>
       {/* right */}
       <div className="flex-[2] bg-[#1f1f1f]">
