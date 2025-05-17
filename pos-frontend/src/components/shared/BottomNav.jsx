bfc2e422e6bb262926fdfd45afabf1b7cf4523ad
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-05-17 10:48:15 +0600

    header and bottom section design done:

---
+++ pos-frontend/src/components/shared/BottomNav.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { FaHome } from "react-icons/fa";
+import { MdOutlineBorderAll, MdTableBar } from "react-icons/md";
+import { CiCircleMore } from "react-icons/ci";
+import { BiSolidDish } from "react-icons/bi";
+
+const BottomNav = () => {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 flex justify-around h-16">
+      <button className="text-[#f5f5f5] cursor-pointer bg-[#343434] w-[200px] rounded-[20px] flex items-center justify-center">
+        <FaHome className="inline mr-4" size={20} />
+        <p>Home</p>
+      </button>
+      <button className="text-[#f5f5f5] cursor-pointer w-[200px] rounded-[20px] flex items-center justify-center">
+        <MdOutlineBorderAll className="inline mr-4" size={20} />
+        <p>Orders</p>
+      </button>
+      <button className="text-[#f5f5f5] cursor-pointer w-[200px] rounded-[20px] flex items-center justify-center">
+        <MdTableBar className="inline mr-4" size={20} />
+        <p>Tables</p>
+      </button>
+      <button className="text-[#f5f5f5] cursor-pointer w-[200px] rounded-[20px] flex items-center justify-center">
+        <CiCircleMore className="inline mr-4" size={20} />
+        <p>More</p>
+      </button>
+
+      <button className="absolute bottom-9 cursor-pointer bg-[#F6B100] text-[#f5f5f5] rounded-full p-3 items-center">
+        <BiSolidDish size={30}/>
+
+      </button>
+    </div>
+  );
+};
+
+export default BottomNav;
--- pos-frontend/src/pages/Home.jsx
+++ pos-frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import BottomNav from '../components/shared/BottomNav'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden'>
+        {/* left */}
+        <div className='flex-[3] bg-[#1a1a1a]'>
+
+        </div>
+        {/* right */}
+        <div className='flex-[2] bg-[#1f1f1f]'>
+
+        </div>
+        <BottomNav/>
+    </section>
   )
 }
 
