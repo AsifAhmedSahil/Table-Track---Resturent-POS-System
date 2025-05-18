09a005fe78e82bf825139020f3026f6109a86e91
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-05-18 23:44:26 +0600

    in progress order page:

--- pos-frontend/src/components/shared/BottomNav.jsx
+++ pos-frontend/src/components/shared/BottomNav.jsx
@@ -3,19 +3,22 @@ import { FaHome } from "react-icons/fa";
 import { MdOutlineBorderAll, MdTableBar } from "react-icons/md";
 import { CiCircleMore } from "react-icons/ci";
 import { BiSolidDish } from "react-icons/bi";
+import {useNavigate} from "react-router-dom"
 
 const BottomNav = () => {
+
+  const navigate = useNavigate()
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 flex justify-around h-16">
-      <button className="text-[#f5f5f5] cursor-pointer bg-[#343434] w-[200px] rounded-[20px] flex items-center justify-center">
+      <button onClick={()=>navigate("/")} className="text-[#f5f5f5] cursor-pointer bg-[#343434] w-[200px] rounded-[20px] flex items-center justify-center">
         <FaHome className="inline mr-4" size={20} />
         <p>Home</p>
       </button>
-      <button className="text-[#f5f5f5] cursor-pointer w-[200px] rounded-[20px] flex items-center justify-center">
+      <button onClick={()=>navigate("/orders")} className="text-[#f5f5f5] cursor-pointer w-[200px] rounded-[20px] flex items-center justify-center">
         <MdOutlineBorderAll className="inline mr-4" size={20} />
         <p>Orders</p>
       </button>
-      <button className="text-[#f5f5f5] cursor-pointer w-[200px] rounded-[20px] flex items-center justify-center">
+      <button onClick={()=>navigate("/tables")} className="text-[#f5f5f5] cursor-pointer w-[200px] rounded-[20px] flex items-center justify-center">
         <MdTableBar className="inline mr-4" size={20} />
         <p>Tables</p>
       </button>
--- pos-frontend/src/pages/Orders.jsx
+++ pos-frontend/src/pages/Orders.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import BottomNav from '../components/shared/BottomNav'
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+     <section className="bg-[#1f1f1f]  h-[calc(100vh-5rem)] overflow-hidden">
+      
+      <BottomNav/>
+     </section>
   )
 }
 
